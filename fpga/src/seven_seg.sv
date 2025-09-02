21e8a1bc14ad0f37e328fd6a85ac9c571da9d31c
Matthew Molinar <mmolinar@hmc.edu>
2025-09-01 18:10:33 -0700

    seven segemnt module

---
+++ fpga/src/seven_seg.sv
@@ -0,0 +1,32 @@
+// author: Matthew Molinar
+// email: mmolinar@hmc.edu
+// date created: 09/1/2025
+
+// 7-segment display
+// seg[0]=A, seg[1]=B, seg[2]=C, seg[3]=D, seg[4]=E, seg[5]=F, seg[6]=G
+module seven_seg(
+    input  logic [3:0] digit,  // Input hex digit
+    output logic [6:0] seg     // 7-segment outputs
+);
+    always_comb begin
+        case (digit)
+            4'h0: seg = 7'b1000000;  // 0: All except G
+            4'h1: seg = 7'b1111001;  // 1: B C
+            4'h2: seg = 7'b0100100;  // 2: A B G E D
+            4'h3: seg = 7'b0110000;  // 3: A B G C D
+            4'h4: seg = 7'b0011001;  // 4: F G B C
+            4'h5: seg = 7'b0010010;  // 5: A F G C D
+            4'h6: seg = 7'b0000010;  // 6: A F G E C D
+            4'h7: seg = 7'b1111000;  // 7: A B C
+            4'h8: seg = 7'b0000000;  // 8: All
+            4'h9: seg = 7'b0010000;  // 9: A B F G C D
+            4'hA: seg = 7'b0001000;  // A: A B C E F G
+            4'hB: seg = 7'b0000011;  // B: C D E F G
+            4'hC: seg = 7'b1000110;  // C: A F E D
+            4'hD: seg = 7'b0100001;  // D: B C D E G
+            4'hE: seg = 7'b0000110;  // E: A F G E D
+            4'hF: seg = 7'b0001110;  // F: A F G E
+            default: seg = 7'b1111111; // None
+        endcase
+    end
+endmodule
--- fpga/src/top.sv
+++ fpga/src/top.sv
@@ -1,23 +1,38 @@
+// author: Matthew Molinar
+// email: mmolinar@hmc.edu
+// date created: 08/31/2025
+
 module top(
-	input 	logic	mcu_blink_in,
-	output 	logic fpga_blink_out, mcu_echo_led
+	input logic clk,
+	input logic [3:0] s,
+	output logic [2:0] led, 
+	output logic [6:0] seg
 );
 
 	logic int_osc;
-	logic pulse;
-	logic led_state = 0;
-	logic [24:0] counter = 0;
+	logic [24:0] counter = 0; // counter for 2.4 Hz
 	
 	// Internal high-speed oscillator
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-	
-	// Simple clock divider
-	always_ff @(posedge int_osc)
-		begin
-			counter <= counter + 1;
-		end
-		
-  assign fpga_blink_out = counter[24];
-	assign mcu_echo_led = mcu_blink_in;
 
-endmodule
+	// Clock divider variable
+    // 48 MHz / 10 MHz = 2.4 Hz
+    parameter CLOCK_DIVIDER = 24'd10000000;
+	
+	// counter
+	always_ff @(posedge int_osc) begin
+        if (counter >= CLOCK_DIVIDER - 1) begin
+            counter <= '0;
+            led[2] <= ~led[2];   // toggle LED at 2.4 Hz
+        end else begin
+            counter <= counter + 1'b1;
+        end
+    end
+
+	led[0] = s[0] & s[1];
+	led[1] = s[2] ^ s[3];
+
+    // seven segment module
+	seven_seg sevseg(.digit(s), .seg(seg));
+
+endmodule 
