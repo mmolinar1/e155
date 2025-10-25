af68c5a4a0bceb740c4f4036e215746cee1a4f1c
Matthew Molinar <mmolinar@hmc.edu>
2025-10-25 12:51:57 -0700

    add round key + tb

--- lab7/lab07_fpga/src/add_round_key.sv
+++ lab7/lab07_fpga/src/add_round_key.sv
@@ -3,3 +3,18 @@
 // date created: 10/24/2025
 
 // add_round_key.sv
+
+/////////////////////////////////////////////
+// add_round_key
+// state and round key are combined by applying
+// a bitwise XOR
+// NIST FIPS 197 section 5.1.4
+/////////////////////////////////////////////
+
+module add_round_key(input  logic [127:0] state_in,
+                     input logic [127:0] round_key
+                     output logic [127:0] state_out);
+
+    assign state_out = state_in ^ key_in;
+
+endmodule
---
+++ lab7/lab07_fpga/src/add_round_key_tb.sv
@@ -0,0 +1,46 @@
+// author: Matthew Molinar
+// email: mmolinar@hmc.edu
+// date created: 10/25/2025
+
+// test bench for add round key module, which
+// is a bitwise XOR
+
+`timescale 1ps/1ps
+
+module shift_rows_tb();
+
+    logic [127:0] state_in, round_key, state_out;
+
+    add_round_key dut(
+        .state_in(state_in),
+        .round_key(round_key)
+        .state_out(state_out)
+    );
+
+    // Start of test
+    
+    // inputs and outputs are based on
+    // the cipher example in Appendix B
+
+    initial begin
+        // Initialize inputs
+        state_in = 128'h3243f6a8885a308d313198a2e0370734;
+        round_key = 128'h2b7e151628aed2a6abf7158809cf4f3c;
+        
+        logic [127:0] expected_state_out = 128'h193de33bf4571d3b2a22ab2ed7344908;
+        
+        #10;
+
+        // Check if output matches the expected result
+        if (state_out === expected_state_out) begin
+            $display("Test passed");
+        end else begin
+            $display("Test failed");
+        end
+
+        // stop the simulation
+        $display("Tests completed");
+        $stop;
+    end
+
+endmodule
